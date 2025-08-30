ba98df825ea0930bb26a7c498c488f30e3089b6f
RoseAurora <roseauroralove@gmail.com>
2025-08-30 16:51:43 +0500

    commit

--- Compiler/Compiler/main.cpp
+++ Compiler/Compiler/main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 using namespace std;
 
+// C++ using C++
+
+#define varTypes = ["int", "float", "string", "char", "bool"];
+#define keywords = ["if", "while", "else", "main", "return"];
+#define operators = ['+', '-', '*', '/', '<', '>', '=', '%'];
+#define seperators = ['{', '}', '[', ']', '(', ')', ';']
+
 int main() {
 	cout << "Real Madrid 15";
 	return 0;
