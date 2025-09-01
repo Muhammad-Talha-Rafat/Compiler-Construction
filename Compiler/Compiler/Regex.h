57735ef8b4f04b6f86b9ca3ccaa850c7c04b4e58
RoseAurora <roseauroralove@gmail.com>
2025-09-01 18:46:21 +0500

    updated Regex.h

--- Compiler/Compiler/Regex.h
+++ Compiler/Compiler/Regex.h
@@ -5,118 +5,163 @@
 #include <regex>
 using namespace std;
 
+struct rule {
+    string type;
+    regex Regex;
+    bool reserved = 1;
+};
+
 struct token {
     string type;
     string value;
+
+    void print() {
+        if (type == "SEMICOLON" || type == "rPARANTHESIS" || type == "lPARANTHESIS") cout << '<' << type << '>' << endl;
+        else if (type == "LIBRARY") cout << '<' << type << ": " << this->value << '>' << endl;
+        else if (value.empty()) cout << '<' << type << '>';
+        else cout << '<' << this->type << ": " << this->value << '>';
+    }
 };
 
+vector<rule> Rules = {
+    {"WHITESPACE", regex("^\\s+")},
+    {"COMMENT", regex("^(//.*|/\\*[^*]*\\*+([^/*][^*]*\\*+)*/)")},
 
-regex whitespace("^\\s+");
-regex comment("^(//.*|/\\*[^*]*\\*+([^/*][^*]*\\*+)*/)");
-regex keyword("^(int|float|double|char|string|bool|void|return|if|else if|else|while|for|do|cout|cin|struct|class|public|private|protected|static|const|using|namespace|endl)");
-regex library("^#include\\s*(<[^>]+>|\"[^\"]+\")");
-regex identifier("^[_A-Za-z][_A-Za-z0-9]*");
-regex number("^[0-9]+");
-regex arithmetic_op("^(\\+\\+|\\-\\-|\\+|\\-|\\*|/|%)");
-regex comparison_op("^(==|!=|<=|>=|>|<)");
-regex assignment_op("^(\\+=|\\-=|\\*=|/=|%=|<<=|>>=|=)");
-regex logical_op("^(&&|\\|\\||!)");
-regex bitwise_op("^(<<|>>|\\^|~|&|\\|)");
-regex lBrace("^\\(");
-regex rBrace("^\\)");
-regex lParenthesis("^\\{");
-regex rParenthesis("^\\}");
-regex lBracket("^\\[");
-regex rBracket("^\\]");
-regex separator("^(;|,)");
-regex strLiteral("^\"([^\"\\\\]|\\\\.)*\"");
+    {"INCLUDE", regex("^#include")},
+    {"LIBRARY", regex("^(<[^>]+>|\"[^\"]+\\.h\")"), 0},
+    {"USING", regex("^using")},
+    {"NAMESPACE", regex("^namespace")},
+    {"DEFINE", regex("^#define")},
 
+    {"INT", regex("^int")},
+    {"FLOAT", regex("^float")},
+    {"DOUBLE", regex("^double")},
+    {"CHAR", regex("^char")},
+    {"STRING", regex("^string")},
+    {"BOOL", regex("^bool")},
+    {"VOID", regex("^void")},
+
+    {"MAIN", regex("^main")},
+    {"RETURN", regex("^return")},
+
+    {"WHILE", regex("^while")},
+    {"DO", regex("^do")},
+    {"FOR", regex("^for")},
+    {"SWITCH", regex("^switch")},
+    {"CASE", regex("^case")},
+    {"DEFAULT", regex("^default")},
+    {"TRY", regex("^try")},
+    {"CATCH", regex("^catch")},
+    {"ELSEIF", regex("^else if")},
+    {"ELSE", regex("^else")},
+
+    {"TEMPLATE", regex("^template")},
+    {"STRUCT", regex("^struct")},
+    {"CLASS", regex("^class")},
+    {"PUBLIC", regex("^public")},
+    {"PRIVATE", regex("^private")},
+    {"PROTECTED", regex("^protected")},
+
+    {"STATIC", regex("^static")},
+    {"CONST", regex("^const")},
+    {"COUT", regex("^cout")},
+    {"CIN", regex("^cin")},
+    {"ENDL", regex("^endl")},
+    {"NEW", regex("^new")},
+    {"DELETE", regex("^delete")},
+    {"SIZEOF", regex("^sizeof")},
+    {"THIS", regex("^this")},
+
+    {"IDENTIFIER", regex("^[_A-Za-z][_A-Za-z0-9]*"), 0},
+
+    {"DECIMAL", regex("^[0-9]+\\.[0-9]+"), 0},
+    {"INTEGER", regex("^[0-9]+"), 0},
+    {"CHARACTER", regex("^'(\\.|[^'\\\\])'"), 0},
+    {"STRLITERAL", regex("^\"([^\"\\\\]|\\\\.)*\""), 0},
+    {"TRUE", regex("^true")},
+    {"FALSE", regex("^false")},
+
+    {"INCREMENT", regex("^\\+\\+")},
+    {"DECREMENT", regex("^\\-\\-")},
+
+    {"EQUALS", regex("^==")},
+    {"NOTEQUAL", regex("^!=")},
+    {"GREATERorEQUAL", regex("^>=")},
+    {"SMALLERorEQUAL", regex("^<=")},
+
+    {"AND_LOGIC", regex("^&&")},
+    {"OR_LOGIC", regex("^\\|\\|")},
+    {"NOT_LOGIC", regex("^!")},
+
+    {"LEFT_SHIFT", regex("^<<")},
+    {"RIGHT_SHIFT", regex("^>>")},
+
+    {"ADD_ASSIGN", regex("^\\+=")},
+    {"SUB_ASSIGN", regex("^\\-=")},
+    {"MUL_ASSIGN", regex("^\\*=")},
+    {"DIV_ASSIGN", regex("^/=")},
+    {"MOD_ASSIGN", regex("^%=")},
+    {"AND_ASSIGN", regex("^&=")},
+    {"OR_ASSIGN", regex("^\\|=")},
+    {"XOR_ASSIGN", regex("^\\^=")},
+    {"LEFT_SHIFT_ASSIGN", regex("^<<=")},
+    {"RIGHT_SHIFT_ASSIGN", regex("^>>=")},
+
+    {"ASSIGN", regex("^=")},
+    {"ADDITION", regex("^\\+")},
+    {"SUBTRACTION", regex("^\\-")},
+    {"MULTIPLICATION", regex("^\\*")},
+    {"DIVISION", regex("^/")},
+    {"MODULUS", regex("^%")},
+
+    {"GREATERTHAN", regex("^>")},
+    {"SMALLERTHAN", regex("^<")},
+
+    {"AND", regex("^&")},
+    {"OR", regex("^\\|")},
+    {"XOR", regex("^\\^")},
+    {"NOT", regex("^~")},
+
+
+    {"QUESTION", regex("^\\?")},
+    {"SCOPE", regex("^::")},
+    {"COLON", regex("^:")},
+    {"SEMICOLON", regex("^;")},
+    {"COMMA", regex("^,")},
+    {"DOT", regex("^\\.")},
+    {"MEMBEROF", regex("^->")},
+
+    {"lBRACE", regex("^\\(")},
+    {"rBRACE", regex("^\\)")},
+    {"lPARANTHESIS", regex("^\\{")},
+    {"rPARANTHESIS", regex("^\\}")},
+    {"lBRACKET", regex("^\\[")},
+    {"rBRACKET", regex("^\\]")}
+};
 
 static vector<token> tokenize(const string& code) {
     vector<token> tokens;
-    int i = 0;
+    size_t i = 0;
 
     while (i < code.size()) {
         string snippet = code.substr(i);
-        smatch substring;
+        smatch match;
+        bool matched = false;
 
-        if (regex_search(snippet, substring, whitespace) && substring.position() == 0) {
-            i += substring.length();
+        for (auto& rule : Rules) {
+            if (regex_search(snippet, match, rule.Regex) && match.position() == 0) {
+                if (rule.type != "WHITESPACE") {
+                    if (rule.reserved) tokens.push_back({ rule.type });
+                    else tokens.push_back({ rule.type, match.str() });
+                }
+                i += match.length();
+                matched = true;
+                break;
+            }
         }
-        else if (regex_search(snippet, substring, comment) && substring.position() == 0) {
-            tokens.push_back({ "COMMENT", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, library) && substring.position() == 0) {
-            tokens.push_back({ "LIBRARY", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, keyword) && substring.position() == 0) {
-            tokens.push_back({ "KEYWORD", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, identifier) && substring.position() == 0) {
-            tokens.push_back({ "IDENTIFIER", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, logical_op) && substring.position() == 0) {
-            tokens.push_back({ "LOGIC", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, bitwise_op) && substring.position() == 0) {
-            tokens.push_back({ "BITWISE", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, assignment_op) && substring.position() == 0) {
-            tokens.push_back({ "ASSIGNMENT", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, comparison_op) && substring.position() == 0) {
-            tokens.push_back({ "COMPARATOR", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, arithmetic_op) && substring.position() == 0) {
-            tokens.push_back({ "OPERATOR", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, number) && substring.position() == 0) {
-            tokens.push_back({ "NUMBER", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, strLiteral) && substring.position() == 0) {
-            tokens.push_back({ "STRING", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, lBrace) && substring.position() == 0) {
-            tokens.push_back({ "L_Brace", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, rBrace) && substring.position() == 0) {
-            tokens.push_back({ "R_Brace", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, lParenthesis) && substring.position() == 0) {
-            tokens.push_back({ "L_PARENTHESIS", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, rParenthesis) && substring.position() == 0) {
-            tokens.push_back({ "R_PARENTHESIS", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, lBracket) && substring.position() == 0) {
-            tokens.push_back({ "L_Bracket", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, rBracket) && substring.position() == 0) {
-            tokens.push_back({ "R_Bracket", substring.str() });
-            i += substring.length();
-        }
-        else if (regex_search(snippet, substring, separator) && substring.position() == 0) {
-            tokens.push_back({ "SEPERATOR", substring.str() });
-            i += substring.length();
-        }
-        else {
-            tokens.push_back({ "UNDEFINED", "?" });
+
+        if (!matched) {
+            tokens.push_back({ "UNDEFINED", string(1, code[i]) });
             i++;
         }
     }
@@ -127,15 +172,12 @@ static void runWithRegex(const string& filename) {
     ifstream file(filename);
 
     string code((istreambuf_iterator<char>(file)), istreambuf_iterator<char>());
+
     file.close();
 
-    if (!code.empty() && code[0] == '\xEF') {
-        code.erase(0, 3);
-    }
+    if (!code.empty() && code[0] == '\xEF') code.erase(0, 3);
 
     vector<token> tokens = tokenize(code);
 
-    for (auto& i : tokens) {
-        cout << '<' << i.type << ": " << i.value << '>' << endl;
-    }
+    for (auto& i : tokens) i.print();
 }
--- Compiler/Compiler/main.cpp
+++ Compiler/Compiler/main.cpp
@@ -1,6 +1,6 @@
 ﻿#include <iostream>
 #include "Regex.h"
-#include "noRegex.h"
+//#include "noRegex.h"
 using namespace std;
 
 int main() {
